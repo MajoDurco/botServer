79e9a637463ebf506ea62f7bc0031756e6c069eb
Marian Durco <majodurco@gmail.com>
2017-04-03 19:56:38 +0200

    Fix c3po

--- bots/c_3po.py
+++ bots/c_3po.py
@@ -2,19 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from chatterbot import ChatBot
-from chatterbot.trainters import ListTrainer
+from chatterbot.trainers import ListTrainer
 
 bot_c_3po = ChatBot("C-3PO",
                     output_adapter="chatterbot.output.OutputAdapter",
                     input_adapter="chatterbot.input.VariableInputTypeAdapter",
                     logic_adapters=[
-                        {'import_path:'"chatterbot.logic.BestMatch"},
-                        {'import_path': 'chatter.logic.LowConfidenceAdapter',
+                        {'import_path':"chatterbot.logic.BestMatch"},
+                        {'import_path': 'chatterbot.logic.LowConfidenceAdapter',
                          'threshold': 0.4,
                          'default_response': '''Don't blame me. I'm an
                          interpreter. I'm not supposed to know a power socket
                          from a computer terminal.'''}
-                    ]
+                    ],
+                    trainer='chatterbot.trainers.ListTrainer'
                     )
 
 bot_c_3po.set_trainer(ListTrainer)
