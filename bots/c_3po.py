02da0b4ec1fe4e3be7ac77e542a39574c960ff91
Marian Durco <majodurco@gmail.com>
2017-04-03 20:03:28 +0200

    Fix default message

--- bots/c_3po.py
+++ bots/c_3po.py
@@ -11,9 +11,7 @@ bot_c_3po = ChatBot("C-3PO",
                         {'import_path':"chatterbot.logic.BestMatch"},
                         {'import_path': 'chatterbot.logic.LowConfidenceAdapter',
                          'threshold': 0.4,
-                         'default_response': '''Don't blame me. I'm an
-                         interpreter. I'm not supposed to know a power socket
-                         from a computer terminal.'''}
+                         'default_response': '''Don't blame me. I'm an interpreter. I'm not supposed to know everything.'''}
                     ],
                     trainer='chatterbot.trainers.ListTrainer'
                     )
